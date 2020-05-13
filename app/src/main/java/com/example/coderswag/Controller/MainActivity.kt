320e9b8211b144ba14a11e4c35c11286d87c3a3a
Nabin <nabin.g14@gmail.com>
2020-05-13 14:22:32 -0400

    Fourth

--- app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.coderswag.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.coderswag.Adapters.CategoryAdapter
 import com.example.coderswag.Model.Category
 import com.example.coderswag.R
@@ -18,7 +21,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         adapter = CategoryAdapter(this, DataService.categories)
-
         categoryListView.adapter = adapter
+
+//        categoryListView.setOnItemClickListener {parent: AdapterView<*>?, view: View?, position: Int, id: Long ->
+//            val category = DataService.categories[position]
+//            Toast.makeText(this, "You clicked on the ${category.title} cell", Toast.LENGTH_SHORT).show()
+//
+//        }
     }
 }
